923285a263ac2a2ac69a92d6789b7aa13bb4d3e8
Alex Morozov <morozovalex842@gmail.com>
2025-06-29 17:10:28 +0300

    fix: correct recipe details endpoint URL formatting

--- src/constants/endpoints.ts
+++ src/constants/endpoints.ts
@@ -4,7 +4,7 @@ const apiKey = process.env.SPOONACULAR_API_KEY;
 
 const endpoints = {
   getRecipeDetails: (id: number) =>
-    `${process.env.BASE_API_URL}${id}recipes/information?apiKey=${apiKey}`,
+    `${process.env.BASE_API_URL}recipes/${id}/information?apiKey=${apiKey}`,
   getRecipeList: ({ query, cuisine, prepTime }: IGetRecipeRequest) =>
     `${process.env.BASE_API_URL}recipes/complexSearch?query=${query}${cuisine ? `&cuisine=${cuisine}` : ''}${prepTime ? `&maxReadyTime=${prepTime}` : ''}&apiKey=${apiKey}`,
 };
